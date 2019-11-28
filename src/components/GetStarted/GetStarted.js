16904cb534083cf2b5332d69a5fbe7b70b42ef3d
jessm12 <jessmccreery@hotmail.co.uk>
2019-11-28 12:09:21 +0000

    Getting started form first draft

--- src/components/GetStarted/GetStarted.js
+++ src/components/GetStarted/GetStarted.js
@@ -1,18 +1,88 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../App.scss';
 import HomepageImage from '../HomepageImage'
+import './GetStarted.scss';
+import { Col, Button, Row, FormText, Form, FormGroup, Label, Input } from 'reactstrap';
+import { YearPicker, MonthPicker, DayPicker } from 'react-dropdown-date';
 
-const GetStarted = () => {
-	return (
-		<>
-			<div className='mainDiv'>
-				<HomepageImage className='homeImg' />
-				<p className='title'>
-					Get started
-				</p>
-			</div>
-		</>
-	);
+class GetStarted extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { year: null, month: null, day: null };
+	}
+
+	render() {
+		return (
+			<>
+				<div className='mainDiv'>
+					<HomepageImage className='homeImg' />
+				</div>
+				<div>
+					<Form className='getStartedForm'>
+						<h5> Before you can get started planning your dream park trip we'll need some info from you!</h5>
+						<Row form>
+							<Col md={6}>
+								<FormGroup>
+									<Label for="email">Email</Label>
+									<Input type="email" name="email" id="exampleEmail" placeholder="example@example.com" />
+									<FormText> We need this so we can email you your plans! </FormText>
+								</FormGroup>
+							</Col>
+						</Row>
+						<Label>Date of Birth</Label>
+						<Row form>
+							<Col md={2}>
+								<FormGroup>
+									<Label for="year">Year</Label>
+									<div>
+										<YearPicker 
+											value={this.state.year}
+											onChange={(year) => {
+                        this.setState({ year });
+                        console.log(year);
+                    	}}
+										/> 
+									</div>
+								</FormGroup>
+							</Col>
+							<Col md={2}>
+								<FormGroup>
+									<Label for="month">Month</Label>
+									<div>
+										<MonthPicker 
+											year={this.state.year}
+											value={this.state.month}
+											onChange={(month) => {
+                        this.setState({ month });
+                        console.log(month);
+                    	}}
+										/> 
+									</div>
+								</FormGroup>
+							</Col>
+							<Col md={2}>
+								<FormGroup>
+									<Label for="day">Day</Label>
+									<div>
+										<DayPicker 
+											year={this.state.year}
+											month={this.state.month}
+											value={this.state.day}
+											onChange={(day) => {
+                        this.setState({ day });
+                        console.log(day);
+                    	}}
+										/>  
+									</div>
+								</FormGroup>
+							</Col>
+						</Row>
+						<Button>Submit</Button>
+					</Form>
+				</div>
+			</>
+		);
+	}
 }
 
 export default GetStarted;
